54d58f9ceb3ba8c440022b63afc74a5582696fbb
Charvia <90585985+Heslanta@users.noreply.github.com>
2023-01-26 20:08:00 +0800

    awal commit

---
+++ js/script.js
@@ -0,0 +1,45 @@
+// toogle class active
+const navbarnav = document.querySelector(".navbar-nav");
+
+//ketiga burger menu di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarnav.classList.toggle("active");
+};
+
+// klik diluar burger untuk hilangkan sidebar
+const hamburger = document.querySelector("#hamburger-menu");
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarnav.contains(e.target)) {
+    navbarnav.classList.remove("active");
+  }
+});
+
+// $(document).ready(function () {
+//   // Add scrollspy to <body>
+//   $("body").scrollspy({ target: ".navbar", offset: 50 });
+
+//   // Add smooth scrolling on all links inside the navbar
+//   $("#myNavbar a").on("click", function (event) {
+//     // Make sure this.hash has a value before overriding default behavior
+//     if (this.hash !== "") {
+//       // Prevent default anchor click behavior
+//       event.preventDefault();
+
+//       // Store hash
+//       var hash = this.hash;
+
+//       // Using jQuery's animate() method to add smooth page scroll
+//       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+//       $("html, body").animate(
+//         {
+//           scrollTop: $(hash).offset().top,
+//         },
+//         800,
+//         function () {
+//           // Add hash (#) to URL when done scrolling (default click behavior)
+//           window.location.hash = hash;
+//         }
+//       );
+//     } // End if
+//   });
+// });
